318e56ac80cc286a9218d6fb6e6a7c5d3cfd6150
brye <bryewalks@gmail.com>
2020-02-06 23:55:55 -0600

    added total_price to computer. refactor this to better handle nil parts

--- lib/pc_parts/computer.rb
+++ lib/pc_parts/computer.rb
@@ -1,3 +1,5 @@
+require_relative './tower.rb'
+
 module PcParts
   class Computer
     attr_accessor :name, :tower, :motherboard, :memory, :cpu, :cpu_cooler, :gpu, :power_supply, :storage
@@ -18,8 +20,21 @@ module PcParts
       motherboard.socket_type == cpu.socket_type
     end
 
+    def parts
+      [tower, motherboard, cpu, cpu_cooler, gpu, memory, storage, power_supply]
+    end
+
     def total_price
-      #add up price of each part
+      sum = 0
+      parts.each do |part|
+        if part && part.price
+          sum += part.price
+        end
+      end
+      sum
     end
   end
 end
+
+comp = PcParts::Computer.new("bryes pc", tower: PcParts::Tower.new(name: "hello"))
+p comp.total_price
